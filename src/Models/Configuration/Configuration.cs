e4f72e9726448e0e4ff77c61dc1644a3244279e1
Andrei Goreglyad <asgoreglyad@gmail.com>
2022-06-27 21:44:26 +0200

    IfElse changed to Switch in Configuration

--- src/Models/Configuration/Configuration.cs
+++ src/Models/Configuration/Configuration.cs
@@ -23,17 +23,19 @@ namespace Exadel.Forecast.Models.Configuration
 
         public void SetDefaultForecastApi(ForecastApi forecastApi)
         {
-            if (forecastApi == ForecastApi.OpenWeather)
+            switch (forecastApi)
             {
-                _defaultRepository = new OpenWeatherRepository(_openWeatherKey);
-            }
-            else if(forecastApi == ForecastApi.WeatherApi)
-            {
-                _defaultRepository = new WeatherapiRepository(WeatherApiKey);
-            }
-            else if(forecastApi == ForecastApi.WeatherBit)
-            {
-                _defaultRepository = new WeatherBitRepository(WeatherBitKey);
+                case ForecastApi.OpenWeather:
+                    _defaultRepository = new OpenWeatherRepository(_openWeatherKey);
+                    break;
+                case ForecastApi.WeatherApi:
+                    _defaultRepository = new WeatherapiRepository(WeatherApiKey);
+                    break;
+                case ForecastApi.WeatherBit:
+                    _defaultRepository = new WeatherBitRepository(WeatherBitKey);
+                    break;
+                default:
+                    break;
             }
         }
 
