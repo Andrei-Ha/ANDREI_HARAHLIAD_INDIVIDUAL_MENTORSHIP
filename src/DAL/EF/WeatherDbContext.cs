d73b32438b5387c76ed5cfafbfa46c9666415f38
Andrei Goreglyad <asgoreglyad@gmail.com>
2022-07-12 09:05:57 +0200

    WeatherDbContext added

---
+++ src/DAL/EF/WeatherDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Exadel.Forecast.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exadel.Forecast.DAL.EF
+{
+    public class WeatherDbContext : DbContext
+    {
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ForecastModel> WeatherModels { get; set; }
+    }
+}
