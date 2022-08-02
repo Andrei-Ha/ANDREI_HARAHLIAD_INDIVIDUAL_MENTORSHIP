a568dea06d23809de88455d7d6cfa878e929dad2
Andrei Goreglyad <asgoreglyad@gmail.com>
2022-08-02 17:08:33 +0200

    UserController added to IS4 without authorization

---
+++ src/IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Exadel.Forecast.IdentityServer.Data;
+using Exadel.Forecast.IdentityServer.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exadel.Forecast.IdentityServer.Controllers
+{
+    [Route("Users")]
+    public class UserController :ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Get([FromQuery] string id)
+        {
+            var users = _dbContext.Users.AsNoTracking();
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                users = users.Where(x => x.Id == id);
+            }
+
+            return new JsonResult(await users.Select(x => new UserDTO { Id = x.Id, UserName = x.UserName, Email = x.Email }).ToListAsync());
+        }
+    }
+}
---
+++ src/IdentityServer/DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Exadel.Forecast.IdentityServer.DTO
+{
+    public class UserDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
